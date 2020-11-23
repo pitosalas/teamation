e1f6f08b1086dda9e5073d81a1fd4995f35411c0
Xiaojing Yan <xyan@brandeis.edu>
2020-11-23 16:04:19 -0500

    comment remove column in preference weights

--- db/migrate/20201111172642_remove_project_from_preference_weights.rb
+++ db/migrate/20201111172642_remove_project_from_preference_weights.rb
@@ -1,5 +1,5 @@
 class RemoveProjectFromPreferenceWeights < ActiveRecord::Migration[6.0]
   def change
-    remove_column :preference_weights, :project, :integer
+    # remove_column :preference_weights, :project, :integer
   end
 end
