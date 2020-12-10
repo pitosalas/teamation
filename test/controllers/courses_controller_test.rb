5fa97f1bcfa860956428f5f7262858b8ccb3e87c
Xiaojing Yan <xyan@brandeis.edu>
2020-12-09 23:37:03 -0500

    add test

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -4,7 +4,6 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
   setup do
     get '/professors/sign_in'
     sign_in users(:one)
-    # @current_user = users(:one)
     post professor_session_url
     @course = courses(:one)
     @current_user = users(:one)
@@ -33,21 +32,15 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
     assert_template "courses/show"
   end
 
-  test "should download the student example file" do
-    # get download_student_list_path
-    # find("Student List Sample CSV").click
-    # wait_for_
-  end
+  # test "should download the student example file" do
+  #   get download_student_list_path
+  #   find("Student List Sample CSV").click
+  # end
 
-  test "should get edit" do
-    get edit_course_url(@course)
-    assert_response :success
-  end
-
-#   test "should update course" do
-#     patch course_url(@course), params: { course: {  } }
-#     assert_redirected_to course_url(@course)
-#   end
+  # test "should get edit" do
+  #   get edit_course_url(@course)
+  #   assert_response :success
+  # end
 
   test "should destroy course" do
     assert_difference('Course.count', -1) do
--- test/controllers/professors_controller_test.rb
+++ test/controllers/professors_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ProfessorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  setup do
+    get '/professors/sign_in'
+    sign_in users(:one)
+    post professor_session_url
+    @professor = users(:one)
+  end
+
+  test "should get index" do
+    get professors_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    sign_out users(:one)
+    get new_professor_registration_url
+    assert_response :success
+  end
+
+  # test "should create professor" do
+  #   assert_difference('Professor.count') do
+  #     post professor_registration_url, params: {:user=> {email: "professor@brandeis.edu", firstname: "admin", 
+  #                                                 lastname: "admin", password: "password", password_confirmation: "password", type: "Professor"}}
+  #   end
+
+  #   # assert_redirected_to professor_url(Professor.last)
   # end
 end
